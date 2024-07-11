976d4eea633e681f4faf1d61bc31f419ca260b8c
Sergey Uchaev <dialoggg1994@gmail.com>
2024-07-11 20:05:13 +0300

    hot fix: add typing

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { getShares } from '@/actions/shares';
 import { SharesList } from '@/components/SharesList';
-import { FinancialInstrument } from '@/typing';
+import { IFinancialInstrument } from '@/typing';
 
 import styles from './page.module.scss';
 
@@ -9,7 +9,7 @@ const Page = async () => {
   const { data } = await getShares();
   return (
     <div className={styles.page}>
-      <SharesList instruments={data.data as FinancialInstrument[]} />
+      <SharesList instruments={data.data as IFinancialInstrument[]} />
     </div>
   );
 };
