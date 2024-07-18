21e00fec03bf1cb5f9de7ba285080840341f93a3
Sergey Uchaev <dialoggg1994@gmail.com>
2024-07-18 22:50:37 +0300

    fix pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Shares } from '@/components/Shares';
 
-import styles from './page.module.scss';
-
 export default function Page({
   searchParams,
 }: {
-  searchParams: { start: string, end: string };
+  searchParams: { start: string; end: string };
 }) {
   return <Shares searchParams={searchParams} />;
 }
