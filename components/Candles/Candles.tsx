70465ac23467b092f090decb34009e335eb3214e
Sergey Uchaev <dialoggg1994@gmail.com>
2024-09-05 00:03:00 +0300

    Refactoring

--- components/Candles/Candles.tsx
+++ components/Candles/Candles.tsx
@@ -1,11 +1,11 @@
 'use client';
 
-import React, { Suspense } from 'react';
+import React from 'react';
 import dynamic from 'next/dynamic';
 import styles from './styles.module.scss';
 const Chart = dynamic(() => import('./Chart/Chart'), { ssr: false });
 
-const Candles = ({ data, currency }: { data: any, currency: string }) => {
+const Candles = ({ data, currency }: { data: any; currency: string }) => {
   return (
     <div className={styles.chart}>
       <Chart data={data} currency={currency} />
