a9d6eb3b12181a22e523a5fe21e2197a647272b8
Sergey Uchaev <dialoggg1994@gmail.com>
2024-09-04 22:49:11 +0300

    Refactoring

--- config/index.ts
+++ config/index.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = 'https://mock.fancy-app.site/api/stock';
-export const BASE_URL = 'https://stock-exchange.fancy-app.site/';
+export const API_BASE_URL = 'https://mock.fancyapp.site/api/stock';
+export const BASE_URL = 'https://stock-exchange.fancyapp.site/';
